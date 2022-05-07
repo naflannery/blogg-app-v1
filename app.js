a4eec2907596c8b9c3a4169fec898bf5d8b82731
naflannery <67547310+naflannery@users.noreply.github.com>
2022-05-07 20:37:09 +0100

    updated to hopefully persist data using mongo

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 //jshint esversion:6
 
+//app require section
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
 const ejs = require("ejs");
-var _ = require("lodash");
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent =
-  "This is Niall Flannery's journal. I plan on updating this journal as and when I do some learning relating to web development or when I post a new project online. Get in touch with any questions, comments or feedback.";
-const aboutContent =
-  "Im a Web Developer, with a passion for magic 🧙‍♂️. I am also a qualified Hypnotherapist.";
+//=============================================================================================================================================
+//============================================= Database setup ================================================================================
+//=============================================================================================================================================
 
-const contactContent = "please get in touch";
+mongoose.connect(
+  "mongodb://localhost:27017/blogDB"
+  //"mongodb+srv://admin-niall:Test123@cluster0.2g8t3.mongodb.net/blog-app"
+);
 
-var posts = [];
+const postSchema = new mongoose.Schema({ title: String, body: String });
 
+const Post = mongoose.model("Post", postSchema);
+
+//=============================================================================================================================================
+//=================================== app.use section and server setup ========================================================================
+//=============================================================================================================================================
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,48 +30,85 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
+//=============================================================================================================================================
+//=================================== static page content ========================================================================
+//=============================================================================================================================================
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+//=============================================================================================================================================
+//=================================== app.get requests ========================================================================
+//=============================================================================================================================================
+
+//home page
+app.get("/", function (req, res) {
+  Post.find({}, (err, posts) => {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      posts: posts,
+    });
   });
 });
 
-app.get("/about", (req, res) => {
+//about page
+app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", (req, res) => {
+//contact page
+app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", (req, res) => {
+//compose page
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", (req, res) => {
-  const postContent = {
-    postTitle: req.body.newTitle,
-    postBody: req.body.newEntry,
-  };
-  posts.push(postContent);
-  res.redirect("/");
+//dynamic requests
+app.get("/posts/:postId", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({
+    _id: requestedPostId,
+    function(err, post) {
+      res.render("post", { title: post.title, body: post.body });
+    },
+  });
 });
 
-app.get("/posts/:entry", (req, res) => {
-  const reqTitle = _.lowerCase(req.params.entry);
+//=============================================================================================================================================
+//=================================== app.post requests ========================================================================
+//=============================================================================================================================================
 
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.postTitle);
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody,
+  });
 
-    if (storedTitle === reqTitle) {
-      const pageTitle = post.postTitle;
-      const pageContent = post.postBody;
-      res.render("post", { pageTitle: pageTitle, pageContent: pageContent });
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
     }
   });
 });
 
+//=============================================================================================================================================
+//=================================== app.listen ========================================================================
+//=============================================================================================================================================
+
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server active");
+  console.log("Server started on port 3000");
 });
+
+// mongo atlas
+// username: admin-niall
+// password: Test123
