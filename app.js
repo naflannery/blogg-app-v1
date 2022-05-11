e668b155e2e35791a742e3ddca75f2da820e4255
naflannery <67547310+naflannery@users.noreply.github.com>
2022-05-11 20:10:13 +0100

    added 'process.env.PORT' insread of requiring port 3000

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
